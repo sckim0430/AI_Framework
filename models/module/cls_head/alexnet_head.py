0496104dcae0c12049c07509b6fc9c0343202454
sckim0430 <another0430@naver.com>
2022-09-07 01:54:40 +0900

    update files

--- models/module/cls_head/alexnet_head.py
+++ models/module/cls_head/alexnet_head.py
@@ -9,18 +9,25 @@ class AlexNet_Head(nn.Module):
         in_channel (int) : number of channel in input feature
         num_class (int) : number of class to classification
         dropout_ratio (float) : dropout ratio
+        pooling_type (str) : option for global average pooling
         loss (dict) : loss option
     """
-    def __init__(self,num_class=1000, dropout_ratio=0.5, loss=dict(type="CrossEntropy")):
+    def __init__(self,num_class=1000, in_channel=256, dropout_ratio=0.5, pooling_type="avg", loss=dict(type="CrossEntropy")):
         self.num_class = num_class
+        self.in_channel = in_channel
         self.dropout_ratio = dropout_ratio
+        self.pooling_type = pooling_type
         # self.loss = build_loss(loss)
 
-        self.fc1 = nn.Linear(6*6*256,4096)
+        self.fc1 = nn.Linear(6*6*self.in_channel,4096)
         self.fc2 = nn.Linear(4096,4096)
         self.fc3 = nn.Linear(4096,1000)
         
-        self.pooling = nn.MaxPool2d(3,2)
+        if self.pooling_type == "avg":
+            self.pooling = nn.AdaptiveAvgPool2d(6)
+        else:
+            self.pooling = nn.MaxPool2d(3,2)
+        
         self.flatten = nn.Flatten()
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(p=self.dropout_ratio)
